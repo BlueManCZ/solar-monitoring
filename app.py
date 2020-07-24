eb22b178b9a794ee08df67544134f90027143528
BlueManCZ <ivo97@centrum.cz>
2020-07-24 22:58:14 +0200

    First prototype

--- app.py
+++ app.py
@@ -1,12 +1,76 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from time import sleep
+from signal import signal, SIGINT
+import threading
+from my_parser import parse
+
+ADDRESS = '0.0.0.0'
+PORT = 5000
 
 app = Flask(__name__)
+app.secret_key = 'secret'
+sio = SocketIO(app)
+connections = 0
+running = True
+
+
+@sio.on('connect')
+def connect():
+    global connections
+    connections += 1
+
+
+@sio.on('disconnect')
+def disconnect():
+    global connections
+    connections -= 1
+
+
+class SolarSystem:
+    panels = []
+    batteries = []
+
+
+class Panel:
+    voltage = 0
+    current = 0
+
+    def power(self):
+        return self.voltage * self.current
+
+
+class Battery:
+    voltage = 0
+    current = 0
+    capacity = 0
+
+
+def auto_refresh():
+    while running:
+        if connections > 0:
+            data = parse()
+            sio.emit('refresh', data, namespace='/')
+        sleep(2)
+
+
+def quit_handler(signal_received, frame):
+    global running
+    running = False
+    quit()
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+
+    signal(SIGINT, quit_handler)
+
+    y = threading.Thread(target=auto_refresh)
+    y.start()
+
+    sio.run(app, host=ADDRESS, port=PORT, debug=True)
---
+++ my_parser.py
@@ -0,0 +1,61 @@
+import urllib3
+import json
+
+
+def fetch_url(url):
+    http = urllib3.PoolManager()
+    resp = http.request('GET', url)
+    return resp.data.decode('utf-8')
+
+
+def parse():
+    site = fetch_url('10.0.0.28')
+
+    voltages_tmp = site.split('U1= ')[1].split('V<br>')[0].replace('V U2= ', '+').split('+')
+    u0 = float(voltages_tmp[0])
+    u1 = float(voltages_tmp[1])
+    panel_voltages = [round(u0-u1, 2), round(u0-u1, 2), round(u0-u1, 2), u1, u1]
+    panel_currents = site.split('I= ')[1].split('A<br>Na')[0].replace('= ', '+').split('+')
+
+    battery_voltages = site.split('Aku: U=')[1].split(', I=')[0].split('V')[:1]
+    battery_currents = site.split(', I=')[1].split('A, ')[:1]
+    battery_capacities = site.split(', I=')[1].split('A, ')[1].split('Ah')[:1]
+
+    supply_voltages = site.split('Zdroj: U=')[1].split(', I=')[0].split('V')[:1]
+    supply_currents = site.split(', I=')[2].split('A<br>')[:1]
+
+    today_energy_global = float(site.split('Dnešní dodávka = <b>')[1].split('Wh')[0])
+    today_energy_network = float(site.split('Do L ')[1].split('Wh')[0])
+    today_power_max = float(site.split('dnes <b>')[1].split('W')[0])
+    overall_power_max = float(site.split('Max výkon <b>')[1].split('W')[0])
+    phase_converter_percentages = int(site.split('[')[1].split('%')[0])
+    phase_power_supply = int(site.split('] ')[1].split(' <br>')[0].split(' ')[3])
+    temperature = int(site.split('Do L')[1].split('°C')[0].split(' ')[2])
+
+    data = {}
+
+    panels = {'U': panel_voltages, 'I': panel_currents}
+    data['panels'] = panels
+
+    batteries = {'U': battery_voltages, 'I': battery_currents, 'C': battery_capacities}
+    data['batteries'] = batteries
+
+    supplies = {'U': supply_voltages, 'I': supply_currents}
+    data['power_supplies'] = supplies
+
+    system = {
+        'TEG': today_energy_global,
+        'TEN': today_energy_network,
+        'TPM': today_power_max,
+        'OPM': overall_power_max,
+        'PCP': phase_converter_percentages,
+        'PPS': phase_power_supply,
+        'T': temperature
+    }
+    data['system'] = system
+
+    json_data = json.dumps(data, indent=4)
+
+    print(json_data)
+
+    return data
